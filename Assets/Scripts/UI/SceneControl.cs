b889693dbbb472677e52e4e4db57a10ca720e064
Scar <43643522+scarcheek@users.noreply.github.com>
2026-07-15 13:49:24 +0200

    Commit before major changes

---
+++ Assets/Scripts/UI/SceneControl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace UI
+{
+    public enum Screen
+    {
+        GameScreen,
+        SettingsScreen,
+        PackagePickerScreen
+    }
+
+    public class SceneControl : MonoBehaviour
+    {
+        [SerializeField] public Screen currentScreen;
+        [SerializeField] public UIManager gameUI;
+        [SerializeField] public UIManager settingsUI;
+        [SerializeField] public UIManager packagePickerUI;
+        
+        public UIManager CurrentUI()
+        {
+            return currentScreen switch
+            {
+                Screen.GameScreen => gameUI,
+                Screen.SettingsScreen => settingsUI,
+                Screen.PackagePickerScreen => packagePickerUI,
+                _ => null
+            };
+        }
+
+        public static SceneControl Instance;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+    }
+}
